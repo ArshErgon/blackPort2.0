d9d613883d34f85fcf8a46e233800b91bbd1f366
ArshErgon <arshergon@gmail.com>
2022-05-15 21:28:24 +0530

    allDone

--- mainPort/models.py
+++ mainPort/models.py
@@ -9,18 +9,22 @@ class ProjectModel(models.Model):
         <div class="swiper mySwiper">
         <div class="swiper-wrapper">
           <div class="swiper-slide">
-            <a href="#modal">
               <img src="https://drive.google.com/uc?export=view&id=1m09zIqXaCEuN_zEwgYgXwVNJ2g5iskFh" alt="" /></a>
-            <center><em>Click or Swipe</em></center>
-          </div>
+            <center><em>School Management</em></center>
           <div class="swiper-slide">
             <img src="https://drive.google.com/uc?export=view&id=1cyW0esSTcYLR-SiUv--Pd5RzW-i0bStG" alt="" />
+            <center><em>Arshskool</em></center>
+
           </div>
           <div class="swiper-slide">
             <img src="https://drive.google.com/uc?export=view&id=1QyxnsKv6smF2NmABYhdx3ci3ZGPX9g_p" alt="" />
+            <center><em>Donation App</em></center>
+
           </div>
           <div class="swiper-slide">
             <img src="https://drive.google.com/uc?export=view&id=11_KCAaYyeNgPUAL7kbPJ1ybuWNzxp43j" alt="" />
+            <center><em>Phishing Google SignIn</em></center>
+
           </div>
         </div>
         <div class="swiper-pagination"></div>
@@ -28,24 +32,7 @@ class ProjectModel(models.Model):
       </div>
     </div>
     </div>
-                    
-                    <! -- here modal box start add and change the name as required -->
-                     <!-- Modal -->
-                    <div class="modal-wrapper" id="modal">
-                    <div class="modal-body card">
-                        <div class="modal-header">
-                            <h2 class="heading">ProjectName</h2>
-                        </div>
-                        <h6>Why does it made?</h6>
-                        <p>this is made for</p>
-                        <h6>Things which made it possible.</h6>
-                        <p>java, html, css</p>
-                        <h6>What you learn?</h6>
-                        <p>I learn deeply</p>
-                    </div>
-                    <a href="#!" class="outside-trigger"></a>
-                </div>
-                    
+               
                     """
     
     
@@ -71,67 +58,7 @@ class LearntModel(models.Model):
 class StyleModel(models.Model):
     styleField = models.TextField(
         default= """        
-<style>
-.modal-header {
-  align-items: baseline;
-  display: flex;
-  justify-content: space-between;
-  z-index: 2;
-  color:white;
-  margin: 0px auto;
-}
 
-
-.modal-wrapper {
-  z-index: 2;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.7);
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  left: 0;
-  position: fixed;
-  right: 0;
-  top: 0;
-  color:white;
-}
-
-#modal {
-    color:white;
-  opacity: 0;
-  transition: opacity 0.25s ease-in-out;
-  visibility: hidden;
-}
-#modal:target {
-  opacity: 1;
-  visibility: visible;
-}
-#modal:target .modal-body {
-  opacity: 1;
-  transform: translateY(1);
-}
-#modal .modal-body {
-  background-color: rgb(31, 32, 32);
-
-  max-width: 500px;
-  opacity: 0;
-  transform: translateY(-100px);
-  transition: opacity 0.25s ease-in-out;
-  width: 100%;
-  z-index: 2;
-
-}
-
-.outside-trigger {
-  bottom: 0;
-  cursor: default;
-  left: 0;
-  position: fixed;
-  right: 0;
-  top: 0;
-}
-
-</style>
 
         """
     )
